16867c5e952a404287a418b1fc86537554d9bbab
Edu Andrade <eduardo.a@edu.pucrs.br>
2023-03-13 20:31:56 -0300

    Funcionando separação de tipos

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import socket
+import struct
 
 ETH_P_ALL = 3
 BUFFSIZE = 1518
 
+
+def ethernet_frame(raw_dados):
+    mac_destino, mac_fonte, protocolo = struct.unpack('! 6s 6s H', raw_dados[:14])
+    return mac_destino, mac_fonte, protocolo
+
+
 sockd = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.htons(ETH_P_ALL))
 
-pack = sockd.recv(BUFFSIZE)
-ethHeader = pack[0][0:14]
-print(ethHeader)
+while True:
+    pack = sockd.recv(BUFFSIZE)
+    mac_dest, mac_orig, tipo = ethernet_frame(pack)
+    if tipo == 0x0800:
+        print('IPv4')
+    elif tipo == 0x0806:
+        print('ARP')
+    elif tipo == 0x86DD:
+        print('IPv6')
