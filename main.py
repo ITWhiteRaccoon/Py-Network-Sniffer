6916bf8259efde382c24414d3a01ead404587acd
Edu Andrade <eduardo.a@edu.pucrs.br>
2023-03-16 15:38:42 -0300

    Trabalhando no IPV4

--- main.py
+++ main.py
@@ -6,21 +6,46 @@ ETH_P_ALL = 3
 BUFFSIZE = 1518
 
 
-class Protocolo(Enum):
+class ProtocoloEthernet(Enum):
     ARP = 0x0806
     IPV4 = 0x0800
     IPV6 = 0x86DD
 
 
+class ProtocoloIPv4(Enum):
+    ICMP = 1
+    IGMP = 2
+    TCP = 6
+    IGRP = 9
+    UDP = 17
+    GRE = 47
+    ESP = 50
+    AH = 51
+    SKIP = 57
+    EIGRP = 88
+    OSPF = 89
+    L2TP = 115
+
+
+class OperacaoARP(Enum):
+    Request = 1
+    Reply = 2
+
+
 def ler_ethernet(pacote):
     mac_destino, mac_origem, protocolo = struct.unpack('!6s 6s H', pacote[:14])
-    pacote = pacote[15:]
-    return mac_destino, mac_origem, protocolo
+    return mac_destino, mac_origem, protocolo, pacote[14:]
 
 
 def ler_arp(pacote):
-    operacao = struct.unpack('!H', pacote[6:8])
+    operacao, = struct.unpack('!H', pacote[6:8])
     return operacao
+
+
+def ler_ipv4(pacote):
+    tamanho, = struct.unpack('!H', pacote[2:4])
+    ttl, protocolo = struct.unpack('!BB', pacote[8:10])
+    return tamanho, ttl, protocolo
 
 
 def main():
@@ -28,19 +53,24 @@ def main():
 
     while True:
         pacote = sockd.recv(BUFFSIZE)
-        mac_dest, mac_orig, protocolo = ler_ethernet(pacote)
+        mac_dest, mac_orig, protocolo_eth, pacote = ler_ethernet(pacote)
         print('\nMAC Dest: ', ':'.join(format(x, '02x') for x in mac_dest))
         print('MAC Orig: ', ':'.join(format(x, '02x') for x in mac_orig))
-        if protocolo == 0x0806:
-            print('Tipo: ARP')
-            operacao = ler_arp(pacote)
-            print(operacao)
-        elif protocolo == 0x0800:
-            print('Tipo: IPv4')
-        elif protocolo == 0x86DD:
-            print('Tipo: IPv6')
-        else:
-            print('Pacote ignorado')
+        match ProtocoloEthernet(protocolo_eth):
+            case ProtocoloEthernet.ARP:
+                print('Tipo: ARP')
+                operacao = OperacaoARP(ler_arp(pacote))
+                print('ARP ', operacao.name)
+            case ProtocoloEthernet.IPV4:
+                print('Tipo: IPv4')
+                tamanho, ttl, protocolo_ipv4 = ler_ipv4(pacote)
+                print('IPv4 ', ProtocoloIPv4(protocolo_ipv4).name)
+                print('TTL ', ttl)
+                print(tamanho, ' bytes')
+            case ProtocoloEthernet.IPV6:
+                print('Tipo: IPv6')
+            case _:
+                print('Pacote ignorado')
         print()
 
 
